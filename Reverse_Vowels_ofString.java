f7901b68d784e00d67fba7c081f549ac7fd11394
imash7x <ashishp2d@gmail.com>
2024-01-02 23:15:13 +0530

    5th

---
+++ Reverse_Vowels_ofString.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        String vowels = "AEIOUaeiou";
+        int start = 0, end = chars.length-1;
+
+        while(start < end){
+            while(start < end && vowels.indexOf(chars[start]) == -1){
+                start++;
+            }
+            while(start < end && vowels.indexOf(chars[end]) == -1){
+                end--;
+            }
+            char ch = chars[start];
+            chars[start] = chars[end];
+            chars[end] = ch;
+            start++;
+            end--;
+        }
+        return new String(chars);
+    }
+}
+class Reverse_Vowels_ofString{
+    public static void main(String[] args){
+        Solution s = new Solution();
+        String result = s.reverseVowels("hello");
+        System.out.println(result);
+    }
+}
