6508020f46fc2fe4c0048ab85c3bc48b2111d334
imash7x <ashishp2d@gmail.com>
2024-01-05 23:04:20 +0530

    7th

---
+++ ProductofArrayExceptSelf.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+
+        prefix[0] = 1;
+        for(int i=1; i<nums.length; i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+        } 
+
+        suffix[suffix.length-1] = 1;
+        for(int i=nums.length-2; i>=0; i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+
+        int[] ans = new int[nums.length];
+        for(int i=0; i<ans.length; i++){
+            ans[i] = prefix[i] * suffix[i];
+        }
+
+        return ans;
+    }
+}
+class ProductofArrayExceptSelf{
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] nums = {2,3,5,1,3};
+        int[] ans = s.productExceptSelf(nums);
+        for(int i=0; i<ans.length; i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
